4b145866aded2a72c5e5ed3d39fda1b40194dc49
24kyle64 <24kyle64@gmail.com>
2014-02-24 14:46:12 -0800

    base commit

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+
+$(document).ready(function(){
+	
+	$.backstretch("images/cigars1.jpg");
+	
+	<!--$('.back').backstretch("images/DKBack.jpg");-->
+	
+$('#map').backstretch("images/map.jpg");
+});
